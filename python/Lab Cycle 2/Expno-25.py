f71f134e2a2528717aa4cf19baa7e37578e67741
Haadi-kp <149859456+Haadi-kp@users.noreply.github.com>
2023-11-23 10:01:31 +0530

    Update Expno-25.py

--- python/Lab Cycle 2/Expno-25.py
+++ python/Lab Cycle 2/Expno-25.py
@@ -1,28 +1,26 @@
 dict1={}
 dict2={}
-n1=int(input("Enter the size of first dictionary"))
-n2=int(input("Enter the size of second dictionary"))
+n1=int(input("Enter the size of first dictionary : "))
 for i in range(n1):
-	key=input("Enter the keys: ")
-	value=input("Enter the values: ")
-	dict1[key]=value
-pirnt(dict1)
+        key=input("Enter the key"+str(i+1)+" : ")
+        value=input("Enter the value"+str(i+1)+" : ")
+        dict1[key]=value
 
+n2=int(input("Enter the size of second dictionary : "))
 for i in range(n2):
-	key=input("Enter the keys: ")
-	value=input("Enter the values: ")
+        key=input("Enter the key"+str(i+1)+" : ")
+        value=input("Enter the values"+str(i+1)+" : ")
         dict2[key]=value
-print(dict2)
 
-#dict1={'a':1,'b':2,'c':3}
-#dict2={'d':4,'e':5,'f':6}
 merged_dict={}
 for k,v in dict1.items():
-	merged_dict[k]=v
+        merged_dict[k]=v
 for k,v in dict2.items():
- 	if k not in merged_dict:
-		merged_dict[k]=v
+        if k not in merged_dict:
+                merged_dict[k]=v
+print()
 print("dictionary 1:",dict1)
+print()
 print("dictionary 2:",dict2)
+print()
 print("merged dictionary:",merged_dict)
-
